4f0f0616909b5542a39bf3a50a9fb96815087b2f
cciacomo <esposito.giacomo2008@gmail.com>
2024-06-18 15:47:16 +0200

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,21 +1,38 @@
 from pytube import YouTube, Playlist
 import os
- 
+
+
 def ChangeExtension(file_path, new_extension):
     base_name, _ = os.path.splitext(file_path)
     new_file_path = base_name + "." + new_extension
     os.rename(file_path, new_file_path)
 
+
+
 def DownloadVideo(link, dirPath) -> None:
     Video = YouTube(link)
     Video.streams.get_highest_resolution().download(dirPath)
 
+
+
 def DownloadAudio(link, dirPath) -> None:
+    index = -1
     Audio = YouTube(link)
     Audio.streams.get_audio_only().download(dirPath)
-    filePath = dirPath + '\\' + Audio.title + '.mp4'
+    name = Audio.title
+    name = name.replace('.','')
+    name = name.replace("'", "")
+    name = name.replace(':', '')
+    name = name.replace('?','')
+    name = name.replace('"','')
+    name = name.replace(';','')
+    name = name.replace(',','')
+    filePath = dirPath + '/' + name + '.mp4'
     ChangeExtension(filePath, 'wav')
 
+
+
 def DownloadPlaylist(link, downloader, dirPath) -> None:
-    for i in link.video_urls:
-            downloader(i, dirPath)
+    pl = Playlist(link)
+    for videos in pl.video_urls:
+            downloader(videos, dirPath)
